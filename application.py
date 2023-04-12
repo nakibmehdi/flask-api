d5ae0447b711c4e83a35282408d6c61bacd68ba5
Mehdi <m.nakib@outlook.fr>
2023-04-12 12:44:22 +0200

    change version

--- application.py
+++ application.py
@@ -9,13 +9,13 @@ def add(a, b):
 
 @application.route('/')
 def main():
-    return 'API V1.0'
+    return 'API V1.1'
 
 
 @application.route('/convert', methods=['GET', 'POST'])
 def convert():
     if request.method == 'GET':
-        return "API V1.0 : /convert [POST]: takes a caracter as input and return  "
+        return "API V1.1 : /convert [POST]: takes a caracter as input and return  "
     elif request.method == "POST":
         res = ast.literal_eval(request.get_json(force=True))
         data = []
