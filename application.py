ce2a1dcdda6f448173624c5cd13e79ad3515f4dc
Mehdi <m.nakib@outlook.fr>
2023-04-12 11:04:16 +0200

    fixing code

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import ast
+application = Flask(__name__)
+
+
+@application.route('/')
+def main():
+    return 'API V1.0'
+
+
+@application.route('/convert', methods=['GET', 'POST'])
+def convert():
+    if request.method == 'GET':
+        return "API V1.0 : /convert [POST]: takes a caracter as input and return  "
+    elif request.method == "POST":
+        res = ast.literal_eval(request.get_json(force=True))
+        data = []
+        for i in res:
+            data.append(ord(i))
+        return str(data)
